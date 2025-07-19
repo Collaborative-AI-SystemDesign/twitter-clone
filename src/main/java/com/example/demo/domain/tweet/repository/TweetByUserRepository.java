35e993dfb08e4184e165774b7e2b282311fae097
724thomas <724thomas@gmail.com>
2025-07-20 02:40:27 +0900

    [MOVE] 경로 수정

--- src/main/java/com/example/demo/domain/tweet/repository/TweetByUserRepository.java
+++ src/main/java/com/example/demo/domain/tweet/repository/TweetByUserRepository.java
@@ -1,5 +1,7 @@
-package com.example.demo.domain.tweet;
+package com.example.demo.domain.tweet.repository;
 
+import com.example.demo.domain.tweet.TweetByUser;
+import com.example.demo.domain.tweet.TweetByUserKey;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.cassandra.repository.Query;
 import org.springframework.stereotype.Repository;
